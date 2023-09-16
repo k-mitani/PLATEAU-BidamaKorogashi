cec223b290379be017e9bea896dc1555ab4c5ae2
k-mitani <k-mitani@unique21.co.jp>
2023-09-17 04:49:11 +0900

    iroiro

---
+++ Assets/Editor/DoWork.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using PLATEAU.Geometries;
+using PLATEAU.Native;
+using UnityEditor;
+using UnityEngine;
+
+public class DoWork : Editor
+{
+    [MenuItem("Tools/SetRoadHeight")]
+    static void SetRoadHeight()
+    {
+        foreach (var obj in Selection.gameObjects)
+        {
+            var mesh = obj.GetComponent<MeshFilter>().sharedMesh;
+            var vertices = mesh.vertices;
+            var targetLayer = 1 << LayerMask.NameToLayer("Floor");
+            for (int i = 0; i < vertices.Length; i++)
+            {
+                Debug.Log(vertices[i]);
+                var ray = new Ray(vertices[i] + Vector3.up * 1000, Vector3.down);
+                Debug.DrawLine(ray.origin, ray.origin + ray.direction * 2000, Color.red, 10);
+                if (Physics.Raycast(ray, out var hit, 99999, targetLayer))
+                {
+                    Debug.Log(hit.collider.name);
+                    vertices[i].y = hit.point.y + 0f;
+                    //vertices[i].y = 0;
+                }
+                else
+                {
+                    Debug.Log("no hit");
+                }
+            }
+            mesh.vertices = vertices;
+            mesh.RecalculateBounds();
+            mesh.RecalculateNormals();
+            mesh.RecalculateTangents();
+        }
+    }
+
+    [MenuItem("Tools/SetDemHeight")]
+    static void SetDemHeight()
+    {
+        foreach (var obj in Selection.gameObjects)
+        {
+            var mesh = obj.GetComponent<MeshFilter>().sharedMesh;
+            var vertices = mesh.vertices;
+            var targetLayer = 1 << LayerMask.NameToLayer("Road");
+            for (int i = 0; i < vertices.Length; i++)
+            {
+                Debug.Log(vertices[i]);
+                var ray = new Ray(vertices[i] + Vector3.up * 1000, Vector3.down);
+                //Debug.DrawLine(ray.origin, ray.origin + ray.direction * 2000, Color.red, 10);
+                if (Physics.Raycast(ray, out var hit, 99999, targetLayer))
+                {
+                    Debug.Log(hit.collider.name);
+                    vertices[i].y = hit.point.y - 0.03f;
+                    //vertices[i].y = 0;
+                }
+                else
+                {
+                    Debug.Log("no hit");
+                }
+            }
+            mesh.vertices = vertices;
+            mesh.RecalculateBounds();
+            mesh.RecalculateNormals();
+            mesh.RecalculateTangents();
+        }
+    }
+
+    [MenuItem("Tools/Calculate")]
+    static void Calculate()
+    {
+        var vec = new PlateauVector3d(
+            50565.8352578886,
+            0,
+            149463.247129192);
+        var geoReference = GeoReference.Create(vec, 1, CoordinateSystem.EUN, 4);
+
+        var kawara = new PlateauVector3d(134.052579999, 0, 34.33882);
+        var coord = geoReference.Unproject(kawara);
+        var kawara2 = new GeoCoordinate(34.33882, 134.052579999, 0);
+        var vec2 = geoReference.Project(kawara2);
+        Debug.Log(vec2);
+    }
+
+}
