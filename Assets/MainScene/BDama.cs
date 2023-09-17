f3ff75d24d2c841a59995b488c6955d71dfd73cc
k-mitani <k-mitani@unique21.co.jp>
2023-09-17 11:16:57 +0900

    最初のセットアップまで

---
+++ Assets/MainScene/BDama.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BDama : MonoBehaviour
+{
+    private Rigidbody rb;
+    [SerializeField] private float jumpForceMax = 100;
+    [SerializeField] public float jumpForceTimeMax = 5;
+
+
+    private void Awake()
+    {
+        TryGetComponent(out rb);
+    }
+
+    public void Jump(float time)
+    {
+        time = Mathf.Clamp(time, 0, jumpForceTimeMax);
+        var force = Mathf.Lerp(0, jumpForceMax, time / jumpForceTimeMax);
+        Debug.Log(time / jumpForceTimeMax);
+        rb.AddForce(Vector3.up * force, ForceMode.Impulse);
+    }
+}
--- Assets/MainScene/UIManager.cs
+++ Assets/MainScene/UIManager.cs
@@ -13,6 +13,12 @@ public class UIManager : MonoBehaviour
     [SerializeField] private TextMeshProUGUI textDirection;
     [SerializeField] private JumpButton buttonJump;
     [SerializeField] private TextMeshProUGUI buttonTextJump;
+    [SerializeField] private TextMeshProUGUI textDestination;
+    [SerializeField] private TextMeshProUGUI textDescription;
+    [SerializeField] private TextMeshProUGUI textAngle;
+    [SerializeField] private GameObject goal;
+
+    [SerializeField] private BDama bdama;
 
     private bool buttonJumpPressing = false;
     private float buttonJumpPressingTime = 0f;
@@ -43,7 +49,8 @@ public class UIManager : MonoBehaviour
 
     public void OnJumpClick(float time)
     {
-        Debug.Log("Jump!" + buttonJumpPressingTime.ToString());
+        Debug.Log("Jump! " + buttonJumpPressingTime.ToString());
+        bdama.Jump(time);
     }
 
 
@@ -51,6 +58,12 @@ public class UIManager : MonoBehaviour
     {
         Instance = this;
         panelMenu.SetActive(false);
+    }
+
+    private void Start()
+    {
+        OnStageStart();
+        StartCoroutine(UpdateDistanceLoop());
     }
 
     private void Update()
@@ -67,10 +80,10 @@ public class UIManager : MonoBehaviour
             else
             {
                 buttonJumpPressingTime += Time.deltaTime;
-                var power = Mathf.Min((int)(buttonJumpPressingTime * 10), 50);
+                var power = Mathf.Min((int)(buttonJumpPressingTime * 10), bdama.jumpForceTimeMax * 10);
                 if ((int)(prevTime * 10) < power)
                 {
-                    buttonTextJump.text = $"ƒWƒƒƒ“ƒv\n({power})";
+                    buttonTextJump.text = $"ã‚¸ãƒ£ãƒ³ãƒ—\n({power})";
                 }
             }
         }
@@ -80,9 +93,57 @@ public class UIManager : MonoBehaviour
             {
                 OnJumpClick(buttonJumpPressingTime);
                 buttonJumpPressing = false;
-                buttonTextJump.text = "ƒWƒƒƒ“ƒv";
+                buttonTextJump.text = "ã‚¸ãƒ£ãƒ³ãƒ—";
             }
         }
     }
 
+
+    private TargetLocation targetLocation;
+    private void OnStageStart()
+    {
+        var target = TargetLocation.Data[Random.Range(0, TargetLocation.Data.Count)];
+        while (target == targetLocation)
+        {
+            target = TargetLocation.Data[Random.Range(0, TargetLocation.Data.Count)];
+        }
+
+        targetLocation = target;
+        textDestination.text = targetLocation.name;
+        textDescription.text = targetLocation.description;
+        goal.transform.position = targetLocation.position;
+    }
+
+    private IEnumerator UpdateDistanceLoop()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(0.3f);
+            if (targetLocation == null) continue;
+
+            var bdamaXZ = new Vector3(bdama.transform.position.x, 0, bdama.transform.position.z);
+            var targetXZ = new Vector3(targetLocation.position.x, 0, targetLocation.position.z);
+            var distance = Vector3.Distance(bdamaXZ, targetXZ);
+            textDistance.text = distance.ToString("N0") + "m";
+
+            var direction = targetXZ - bdamaXZ;
+            var angle = Vector3.SignedAngle(Vector3.forward, direction, Vector3.up);
+            Debug.Log(angle);
+
+            var START = -22.5f;
+            var DIFF = 360 / 8;
+            var text = angle.ToString("N0");
+            if (angle > START + DIFF * 0 && angle < START + DIFF * 1) text = "åŒ—";
+            else if (angle > START + DIFF * 1 && angle < START + DIFF * 2) text = "åŒ—æ±";
+            else if (angle > START + DIFF * 2 && angle < START + DIFF * 3) text = "æ±";
+            else if (angle > START + DIFF * 3 && angle < START + DIFF * 4) text = "å—æ±";
+            else if (angle > START + DIFF * 4 && angle < START + DIFF * 5) text = "å—";
+            else if (angle + 360 > START + DIFF * 4 && angle + 360 < START + DIFF * 5) text = "å—";
+            else if (angle + 360 > START + DIFF * 5 && angle + 360 < START + DIFF * 6) text = "å—è¥¿";
+            else if (angle + 360 > START + DIFF * 6 && angle + 360 < START + DIFF * 7) text = "è¥¿";
+            else if (angle + 360 > START + DIFF * 7 && angle + 360 < START + DIFF * 8) text = "åŒ—è¥¿";
+            textDirection.text = text;
+            textAngle.text = angle.ToString("N0") + "";
+        }
+    }
 }
